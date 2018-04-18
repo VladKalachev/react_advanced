dc8f170ad059e17cf9ab91c6a26130d601c4ccc7
Corvax <vlvete1990@yandex.ru>
2018-04-18 14:19:14 +0300

    v0.0.2

---
+++ src/components/Root.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Root extends Component {
+    static defaultProps = {
+
+    }
+
+    render() {
+        return (
+            <div>Hello World</div>
+        );
+    }
+}
+
+export default Root;
+
+
